f6dd9aa0830001046f1370bbf631297454c54e00
suineg <suineg@inbox.ru>
2018-04-09 00:10:27 +0500

    1
    1

---
+++ send_email.php
@@ -0,0 +1,21 @@
+<?php
+//проверяем, существуют ли переменные в массиве POST
+echo $_POST['name'];
+echo $_POST['phone'];
+if(isset($_POST['name']) and isset($_POST['phone'])){
+    $fio = $_POST['name'];
+    echo $fio; 
+    $tel = $_POST['phone'];
+    $fio = htmlspecialchars($fio);
+    $tel = htmlspecialchars($tel);
+    $fio = urldecode($fio);
+    $tel = urldecode($tel);
+    $fio = trim($fio);
+    $tel = trim($tel);
+
+    if (mail("suinegne@gmail.com", "Заявка с сайта", "Имя:".$fio.". тел: ".$tel ,"From: suineg@inbox.ru \r\n")){        
+        echo "Сообщение успешно отправлено"; 
+    } else { 
+        echo "При отправке сообщения возникли ошибки";
+    }
+}
