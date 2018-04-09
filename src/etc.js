b1271ebf09c289fcf47362516259f6b6f8aee2ce
suineg <suineg@inbox.ru>
2018-04-10 02:52:28 +0500

    Revert "форма готова"
    This reverts commit 0335f9cab644e12295af5d012d4675601832e40e.

--- src/etc.js
+++ src/etc.js
@@ -5,6 +5,29 @@ $(document).ready(function() {
         $('html, body').animate({ scrollTop: $(target).offset().top - 70 }, 800);
         return false;
     });
+<<<<<<< HEAD
+=======
+
+    document.getElementById('feedback-form').addEventListener('submit', function(evt) {
+        var http = new XMLHttpRequest(),
+            f = this;
+        evt.preventDefault();
+        http.open("POST", "contacts.php", true);
+        http.onreadystatechange = function() {
+            if (http.readyState == 4 && http.status == 200) {
+                alert(http.responseText);
+                if (http.responseText.indexOf(f.nameFF.value) == 0) { // очистить поле сообщения, если в ответе первым словом будет имя отправителя
+                    f.messageFF.removeAttribute('value');
+                    f.messageFF.value = '';
+                }
+            }
+        }
+        http.onerror = function() {
+            alert('Извините, данные не были переданы');
+        }
+        http.send(new FormData(f));
+    }, false);
+>>>>>>> parent of 0335f9c... форма готова
 });
 
 $(window).scroll(function() {
